97b1bc9a805b98fbcdcccb12f6bbc940619a0acc
= <=>
2020-06-20 19:24:26 -0400

    Added some functionality to the functions in esc.cpp

--- Hardware/esc.cpp
+++ Hardware/esc.cpp
@@ -1,3 +1,18 @@
 #include "esc.h"
 
+void Esc::Esc(int pin) {
+	Esc::setPin(pin);
+}
 
+void Esc::setSpeed(int speed) {
+	Esc::speed = speed;
+}
+
+void Esc::setPin(int pin) {
+	Esc::pin = pin;
+}
+
+void Esc::init() {
+	
+	
+}
